06f4b08b3443a84c906176b284c39106ea0765ad
Shayan Mandal <138359590+Shayan704@users.noreply.github.com>
2024-10-12 19:59:15 +0530

    changes made3

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -7,41 +7,43 @@ class PredictPipeline:
     def __init__(self):
         pass
 
-    def predict(self,features):
+    def predict(self, features):
         try:
-            model_path='artifacts\model.pkl'
-            preprocessor_path='artifacts\preprocessor.pkl'
-            model=load_object(file_path=model_path)
-            preprocessor=load_object(file_path=preprocessor_path)
-            data_scaled=preprocessor.transform(features)
-            preds= model.predict(data_scaled)
+            # Use forward slashes for path compatibility
+            model_path = 'artifacts/model.pkl'
+            preprocessor_path = 'artifacts/preprocessor.pkl'
+            
+            # Load model and preprocessor
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            # Transform features and predict
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
             return preds
+        
+        except FileNotFoundError as fnf_error:
+            raise CustomException(f"File not found: {fnf_error}", sys)
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomException(e, sys)
 
 
 class CustomData:
     def __init__(self,
-        gender: str,
-        race_ethnicity: str,
-        parental_level_of_education,
-        lunch: str,
-        test_preparation_course: str,
-        reading_score: int,
-        writing_score: int):
-
+                 gender: str,
+                 race_ethnicity: str,
+                 parental_level_of_education: str,
+                 lunch: str,
+                 test_preparation_course: str,
+                 reading_score: int,
+                 writing_score: int):
+        
         self.gender = gender
-
         self.race_ethnicity = race_ethnicity
-
         self.parental_level_of_education = parental_level_of_education
-
         self.lunch = lunch
-
         self.test_preparation_course = test_preparation_course
-
         self.reading_score = reading_score
-
         self.writing_score = writing_score
 
     def get_data_as_data_frame(self):
@@ -59,6 +61,4 @@ class CustomData:
             return pd.DataFrame(custom_data_input_dict)
         
         except Exception as e:
-            raise CustomException(e,sys)
-
-
+            raise CustomException(e, sys)
